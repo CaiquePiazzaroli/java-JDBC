b1363ea57b32f379850c4a4b8cf3bbbe0aa9a06b
caique <caique405@gmail.com>
2026-05-25 16:20:05 -0300

    First commit: Conexao com banco de dados mysql

---
+++ src/main/java/DadosBanco.java
@@ -0,0 +1,5 @@
+public class DadosBanco {
+    public static String url = "jdbc:mysql://localhost:3306/testejava";
+    public static String usuario = "root";
+    public static String senha = "Mysql@010203";
+}
